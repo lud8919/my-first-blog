92758c2ee267ebfb5e94138eedefaf58f1bd42b2
lud8919 <lud8919@gmail.com>
2017-03-16 23:45:37 +0100

    Trzeci commit

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 # Create your views here.
 # Widok jest miejscem, w którym umieszczamy "logikę" naszej aplikacji. Pobierze on informacje od modelu, który wcześniej utworzyłaś, a następnie przekaże je do szablonu (template).
 
-from django.shortcuts import render
 
+
+from django.shortcuts import render
+from django.utils import timezone  #Czas jest potrzebny bo wyświatlamy tylko opublikowane posty a te mają datę publikacji
+from .models import Post    #By modele zapisane w bazie danych się wyświetliły na stronie musimy pobrać te modele i przekazać je do szablonu. Więc tutaj importujemy nazwę modelu (Post) z pliku models.py (kropka oznacza bieżący katalog)
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})  #to jest metoda (def) o nazwie post_list, która przyjmuje zapytanie (request) i zwraca (return) metodę zwaną render, której zadaniem jest wyrenderowanie (złożenie w całość) naszego szablonu blog/post_list.html.
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #Ten QuerySet zwraca listę wpisów, które zostały opublikowane (mają datę publikacji) i są uporządkowane według daty publikacji (published_date). Tu więcej o QuerySet https://docs.djangoproject.com/en/1.8/ref/models/querysets/
+    return render(request, 'blog/post_list.html', {'posts': posts})  #to jest funkcja (def) o nazwie post_list, która przyjmuje zapytanie (request) i zwraca (return) funkcję zwaną render, której zadaniem jest wyrenderowanie (złożenie w całość) naszego szablonu blog/post_list.html.
+
 
 
